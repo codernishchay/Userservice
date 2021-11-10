a5919227b2f872931f43f92c6558a646247d7ea9
Nishchay <56715777+codernishchay@users.noreply.github.com>
2021-11-11 00:00:12 +0530

    Create db.go

---
+++ db.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var Database *mongo.Database
+var DBclient *mongo.Client
+
+func DBConnect() {
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(" "))
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Connected to database")
+	defer client.Disconnect(ctx)
+	DBclient = client
+	Database = client.Database("userdb")
+}
